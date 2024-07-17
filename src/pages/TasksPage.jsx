225a92eb013d20921bdb84798e397d872849c0c8
Gonzalo Billia <gonzalobillia@gmail.com>
2024-07-17 20:19:42 -0300

    retoque loading

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -14,7 +14,7 @@ const TasksPage = () => {
             
             <div className='grid grid-cols-2 lg:grid-cols-3 py-8 px-16 gap-4 h-auto'>
                 {tasks.length === 0 ? 
-                <div className='flex justify-center items-center col-span-2 lg:col-span-3 h-screen'><h3>loading...</h3></div> : 
+                <div className='flex justify-center items-center col-span-2 lg:col-span-3 max-h-screen'><h3>No Tasks</h3></div> : 
                 tasks.map((task) => (
                     <TaskCard key={task._id} task={task} />
                 ))}

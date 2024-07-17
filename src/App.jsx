82cf883c1b3e3742726dfb295b3b1e2094fd011a
Gonzalo Billia <gonzalobillia@gmail.com>
2024-07-17 12:12:47 -0300

    configuro ruteo basico en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App(){
   return(
-    <div>
-      <h1 className="text-3xl font-bold">Hello World</h1>
-    </div>
+    <BrowserRouter>
+
+    <Routes>
+
+      <Route path="/" element={<h1 className="text-3xl font-bold">Home Page</h1>} />
+
+      <Route path="/login" element={<h1 className="text-3xl font-bold">Login</h1>} />
+
+      <Route path="/register" element={<h1 className="text-3xl font-bold">Register</h1>} />
+
+      <Route path="/tasks" element={<h1 className="text-3xl font-bold">Tasks Page</h1>} />
+
+      <Route path="/add-task" element={<h1 className="text-3xl font-bold">New Task</h1>} />
+
+      <Route path="/tasks/:id" element={<h1 className="text-3xl font-bold">Update Task</h1>} />
+
+      <Route path="/profile" element={<h1 className="text-3xl font-bold">Profile</h1>} />
+
+    </Routes>
+
+    </BrowserRouter>
   )
 }
 

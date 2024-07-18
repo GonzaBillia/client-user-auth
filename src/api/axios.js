f1badc63f7626c106c9e65d45c88173086bf3ef8
Gonzalo Billia <gonzalobillia@gmail.com>
2024-07-17 21:27:50 -0300

    coloco puerto 3000

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance =axios.create({
-    baseURL: 'https://backend-user-auth-16xe.onrender.com/api',
+    baseURL: 'https://backend-user-auth-16xe.onrender.com:3000/api',
     withCredentials: true
 })
 

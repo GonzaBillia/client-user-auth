a73c317880540dadf8c55e28e17b999bf0546915
Gonzalo Billia <gonzalobillia@gmail.com>
2024-07-17 22:31:40 -0300

    retoques y aviso de cookie en homepage

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# React + Vite
+# Task Manager
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Proyect build for practising backend basic skills
 
-Currently, two official plugins are available:
+the backend main proyect is in: https://github.com/GonzaBillia/backend-user-auth
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+This proyect is just a basic UI for a confortable use of the backend proyect
+
+You can directly go to the REPO or the Hosting Page for use: https://backend-user-auth-16xe.onrender.com
+
+
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <div className='container mx-auto w-full flex justify-center items-center h-[calc(100vh-150px)]'>
+        <div className='container mx-auto w-full flex flex-col justify-center items-center h-[calc(100vh-150px)]'>
+            <div className='bg-red-500 w-[500px] rounded-md p-10'>
+                <h2 className='font-bold pb-8'>Delete the Cookie when Logout to Register or Login with a Diferent Account</h2>
+                <p>Web Browsers may block cookies from third-party websites.</p>
+            </div>
             <div className='flex flex-col justify-center items-center bg-zinc-800 rounded-md p-10'>
                 <h1 className='text-4xl font-bold pb-8'>Welcome to Task Manager</h1>
                 <p>Register or Login to Start</p>
